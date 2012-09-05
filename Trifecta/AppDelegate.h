bb4294d14149da081840d1ae8773b018a950314b
Ran Tao <ran@ephexi.com>
2012-09-05 15:55:24 -0700

    Initial Commit

---
+++ Trifecta/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Trifecta
+//
+//  Created by Ran Tao on 9.5.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Trifecta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Trifecta
+//
+//  Created by Ran Tao on 9.5.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
